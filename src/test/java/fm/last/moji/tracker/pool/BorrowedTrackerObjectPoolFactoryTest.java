6f1cb0e3d20d88f4034a43c7fbdad8f629ba5d65
Elliot West <teabot@gmail.com>
2012-06-08 13:59:21 +0100

    Tests for BorrowedTrackerObjectPoolFactory

---
+++ src/test/java/fm/last/moji/tracker/pool/BorrowedTrackerObjectPoolFactoryTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2012 Last.fm
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package fm.last.moji.tracker.pool;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.InetSocketAddress;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import fm.last.moji.tracker.Tracker;
+import fm.last.moji.tracker.TrackerException;
+import fm.last.moji.tracker.impl.AbstractTrackerFactory;
+import fm.last.moji.tracker.impl.CommunicationException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BorrowedTrackerObjectPoolFactoryTest {
+
+  @Mock
+  private AbstractTrackerFactory mockTrackerFactory;
+  @Mock
+  private MultiHostTrackerPool mockTrackerPool;
+  @Mock
+  private ManagedTrackerHost mockManagedHost;
+  @Mock
+  private BorrowedTracker mockBorrowedTracker;
+  @Mock
+  private Tracker mockTracker;
+
+  private BorrowedTrackerObjectPoolFactory factory;
+  private InetSocketAddress socketAddress;
+
+  @Before
+  public void setup() {
+    factory = new BorrowedTrackerObjectPoolFactory(mockTrackerFactory, mockTrackerPool);
+    when(mockBorrowedTracker.getHost()).thenReturn(mockManagedHost);
+    socketAddress = new InetSocketAddress("localhost", 17476);
+  }
+
+  @Test
+  public void validateObjectSuccess() throws TrackerException {
+    boolean isValid = factory.validateObject(mockManagedHost, mockBorrowedTracker);
+    assertThat(isValid, is(true));
+    verify(mockBorrowedTracker).noop();
+  }
+
+  @Test
+  public void validateObjectFailures() throws TrackerException {
+    doThrow(new TrackerException()).when(mockBorrowedTracker).noop();
+    boolean isValid = factory.validateObject(mockManagedHost, mockBorrowedTracker);
+    assertThat(isValid, is(false));
+  }
+
+  @Test
+  public void destroyObjectNormal() throws Exception {
+    factory.destroyObject(mockManagedHost, mockBorrowedTracker);
+    verify(mockBorrowedTracker).reallyClose();
+  }
+
+  @Test
+  public void destroyObjectFailed() throws Exception {
+    when(mockBorrowedTracker.getLastException()).thenReturn(new CommunicationException());
+    factory.destroyObject(mockManagedHost, mockBorrowedTracker);
+    verify(mockManagedHost).markAsFailed();
+    verify(mockBorrowedTracker).reallyClose();
+  }
+
+  @Test
+  public void makeObject() throws Exception {
+    when(mockTrackerFactory.newTracker(socketAddress)).thenReturn(mockTracker);
+    when(mockManagedHost.getAddress()).thenReturn(socketAddress);
+    BorrowedTracker newTracker = (BorrowedTracker) factory.makeObject(mockManagedHost);
+    newTracker.noop();
+
+    assertThat(newTracker.getHost(), is(mockManagedHost));
+    verify(mockTracker).noop();
+  }
+
+}
--- src/test/java/fm/last/moji/tracker/pool/MultiHostTrackerPoolTest.java
+++ src/test/java/fm/last/moji/tracker/pool/MultiHostTrackerPoolTest.java
@@ -41,13 +41,13 @@ import fm.last.moji.tracker.impl.CommunicationException;
 public class MultiHostTrackerPoolTest {
 
   @Mock
-  private NetworkingConfiguration netConfig;
+  private NetworkingConfiguration mockNetConfig;
   @Mock
-  private GenericKeyedObjectPool pool;
+  private GenericKeyedObjectPool mockPool;
   @Mock
-  private ManagedTrackerHost managedHost1;
+  private ManagedTrackerHost mockManagedHost1;
   @Mock
-  private ManagedTrackerHost managedHost2;
+  private ManagedTrackerHost mockManagedHost2;
   @Mock
   private InetSocketAddress mockAddress1;
   @Mock
@@ -62,18 +62,18 @@ public class MultiHostTrackerPoolTest {
 
   @Before
   public void setup() {
-    when(managedHost1.getAddress()).thenReturn(mockAddress1);
-    when(managedHost2.getAddress()).thenReturn(mockAddress2);
-    managedHosts = Arrays.asList(managedHost1, managedHost2);
+    when(mockManagedHost1.getAddress()).thenReturn(mockAddress1);
+    when(mockManagedHost2.getAddress()).thenReturn(mockAddress2);
+    managedHosts = Arrays.asList(mockManagedHost1, mockManagedHost2);
 
-    when(mockBorrowedTracker1.getHost()).thenReturn(managedHost1);
-    when(mockBorrowedTracker2.getHost()).thenReturn(managedHost2);
-    trackerPool = new MultiHostTrackerPool(managedHosts, netConfig, pool);
+    when(mockBorrowedTracker1.getHost()).thenReturn(mockManagedHost1);
+    when(mockBorrowedTracker2.getHost()).thenReturn(mockManagedHost2);
+    trackerPool = new MultiHostTrackerPool(managedHosts, mockNetConfig, mockPool);
   }
 
   @Test(expected = CommunicationException.class)
   public void getTrackerConvertsException() throws Exception {
-    when(pool.borrowObject(any(ManagedTrackerHost.class))).thenThrow(new IllegalStateException());
+    when(mockPool.borrowObject(any(ManagedTrackerHost.class))).thenThrow(new IllegalStateException());
     trackerPool.getTracker();
   }
 
@@ -86,18 +86,18 @@ public class MultiHostTrackerPoolTest {
 
   @Test
   public void expectedHostPreference() throws Exception {
-    when(managedHost1.getLastUsed()).thenReturn(2L);
-    when(managedHost1.getLastFailed()).thenReturn(1L);
-    when(managedHost2.getLastUsed()).thenReturn(2L);
-    when(managedHost2.getLastFailed()).thenReturn(0L);
+    when(mockManagedHost1.getLastUsed()).thenReturn(2L);
+    when(mockManagedHost1.getLastFailed()).thenReturn(1L);
+    when(mockManagedHost2.getLastUsed()).thenReturn(2L);
+    when(mockManagedHost2.getLastFailed()).thenReturn(0L);
 
-    when(pool.borrowObject(managedHost1)).thenReturn(mockBorrowedTracker1);
-    when(pool.borrowObject(managedHost2)).thenReturn(mockBorrowedTracker2);
+    when(mockPool.borrowObject(mockManagedHost1)).thenReturn(mockBorrowedTracker1);
+    when(mockPool.borrowObject(mockManagedHost2)).thenReturn(mockBorrowedTracker2);
 
     BorrowedTracker first = (BorrowedTracker) trackerPool.getTracker();
     assertThat(first, is(mockBorrowedTracker2));
 
-    when(managedHost2.getLastFailed()).thenReturn(10L);
+    when(mockManagedHost2.getLastFailed()).thenReturn(10L);
     BorrowedTracker second = (BorrowedTracker) trackerPool.getTracker();
     assertThat(second, is(mockBorrowedTracker1));
   }
@@ -105,19 +105,19 @@ public class MultiHostTrackerPoolTest {
   @Test
   public void invalidateTracker() throws Exception {
     trackerPool.invalidateTracker(mockBorrowedTracker1);
-    verify(pool).invalidateObject(managedHost1, mockBorrowedTracker1);
+    verify(mockPool).invalidateObject(mockManagedHost1, mockBorrowedTracker1);
   }
 
   @Test
   public void returnTracker() throws Exception {
     trackerPool.returnTracker(mockBorrowedTracker1);
-    verify(pool).returnObject(managedHost1, mockBorrowedTracker1);
+    verify(mockPool).returnObject(mockManagedHost1, mockBorrowedTracker1);
   }
 
   @Test
   public void closeDelegates() throws Exception {
     trackerPool.close();
-    verify(pool).close();
+    verify(mockPool).close();
   }
 
 }
